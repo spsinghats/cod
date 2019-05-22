7c32fab493991ec36592423937f3fa3cb92d7a91
vaibhav <vaibhav.sharma@athenasoft.in>
2019-05-22 17:04:45 +0530

    remove code

--- src/Providers/ButtonProvider.php
+++ src/Providers/ButtonProvider.php
@@ -9,85 +9,11 @@
 namespace Ceevo\Providers;
 
 use Plenty\Plugin\Templates\Twig;
-use Plenty\Modules\Basket\Models\Basket;
-use Plenty\Plugin\ConfigRepository;
-use Plenty\Modules\Item\Item\Contracts\ItemRepositoryContract;
-use Plenty\Modules\Basket\Models\BasketItem;
-use Plenty\Modules\Account\Address\Models\Address;
-use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
-use Plenty\Modules\Account\Address\Contracts\AddressRepositoryContract;
-use Plenty\Modules\Order\Shipping\Countries\Contracts\CountryRepositoryContract;
-use Plenty\Modules\Payment\Events\Checkout\GetPaymentMethodContent;
-use Plenty\Modules\Helper\Services\WebstoreHelper;
-use Plenty\Plugin\Log\Loggable;
-use Plenty\Modules\Payment\Method\Models\PaymentMethod;
-use Plenty\Modules\Order\Models\Order;
-use Plenty\Modules\Order\Contracts\OrderRepositoryContract;
-use Plenty\Modules\Item\Variation\Contracts\VariationRepositoryContract;
-use Plenty\Modules\Order\RelationReference\Models\OrderRelationReference;
 
 
 class ButtonProvider
 {
-    use Loggable;
-    /**
-     *
-     * @var ConfigRepository
-     */
-    private $config;
-    /**
-     *
-     * @var ItemRepositoryContract
-     */
-    private $itemRepository;
-    /**
-     *
-     * @var VariationRepositoryContract
-     */
-    private $variationRepository;
-    /**
-     *
-     * @var FrontendSessionStorageFactoryContract
-     */
-    private $session;
-    /**
-     *
-     * @var AddressRepositoryContract
-     */
-    private $addressRepository;
-    /**
-     *
-     * @var CountryRepositoryContract
-     */
-    private $countryRepository;
-    /**
-     *
-     * @var WebstoreHelper
-     */
-    private $webstoreHelper;
-    /**
-     *
-     * @var OrderHelper
-     */
-    private $orderHelper;
-    /**
-     *
-     * @var OrderRepositoryContract
-     */
-    private $orderRepository;
-
-    public function __construct( ConfigRepository $config, ItemRepositoryContract $itemRepository, VariationRepositoryContract $variationRepository, FrontendSessionStorageFactoryContract $session, AddressRepositoryContract $addressRepository, CountryRepositoryContract $countryRepository, WebstoreHelper $webstoreHelper,  OrderRepositoryContract $orderRepository)
-    {
-        $this->config = $config;
-        $this->itemRepository = $itemRepository;
-        $this->variationRepository = $variationRepository;
-        $this->session = $session;
-        $this->addressRepository = $addressRepository;
-        $this->countryRepository = $countryRepository;
-        $this->webstoreHelper = $webstoreHelper;
-        $this->orderRepository = $orderRepository;
-    }
-
+    
     public function call(Twig $twig):string
     {
         $get_data = $this->callAPI('GET', 'https://api.ceevo.com/acquiring/methods', []);
@@ -97,16 +23,14 @@ class ButtonProvider
             array_push($methods_array,$methods['title']);
 
         }
-        $this->getLogger(__METHOD__)->error('inside button provider', $this->config);
-        $apiKey = $this->config->get('Ceevo.apiKey');
+        //$apiKey = $this->config->get('Ceevo.apiKey');
         $templateData = array(
-            'methods' => $methods_array,
-            'apiKey' => $apiKey
+            'methods' => $methods_array
         );
         return $twig->render('Ceevo::Icon',$templateData);
     }
 
-    public function callAPI($method, $url, $data){
+    function callAPI($method, $url, $data){
         $curl = curl_init();
      
         switch ($method){
