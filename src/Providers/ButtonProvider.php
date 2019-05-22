9142923a6e338185a20524c5acbbc28fb87996fb
vaibhav <vaibhav.sharma@athenasoft.in>
2019-05-22 16:16:30 +0530

    chnage

--- src/Providers/ButtonProvider.php
+++ src/Providers/ButtonProvider.php
@@ -15,7 +15,7 @@ class ButtonProvider
 {
     public function call(Twig $twig):string
     {
-        $get_data = $this->callAPI('GET', 'https://api.ceevo.com/acquiring/methods', false);
+        $get_data = $this->callAPI('GET', 'https://api.ceevo.com/acquiring/methods', []);
         $response = json_decode($get_data, true);
         $methods_array = [];
         foreach($response as $methods){
