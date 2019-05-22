822228552444f38d5e023cbbbf8a79505a13886d
vaibhav <vaibhav.sharma@athenasoft.in>
2019-05-22 13:59:36 +0530

    registerJs in Plugin

---
+++ src/Providers/CeevoJsProvider.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: ckunze
+ * Date: 8/3/17
+ * Time: 18:27
+ */
+
+namespace CashOnDelivery\Providers;
+
+use Plenty\Plugin\Templates\Twig;
+
+
+class CeevoJsProvider
+{
+    public function call(Twig $twig):string
+    {
+        return $twig->render('Ceevo::Js');
+    }
+
+}
